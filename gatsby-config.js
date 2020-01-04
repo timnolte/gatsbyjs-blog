bd5241526d6eb31a9530088bf19a845c7ddd64ff
tim.nolte <tim.nolte@ndigitals.com>
2020-01-03 23:37:14 -0500

    * Changes to WordPress starter.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,38 @@
 module.exports = {
   siteMetadata: {
-    title: `Tim's #100DaysOfGatsby Blog`,
-    description: `This site is intended to fullfill Challenge #1 during the #100DaysOfGatsby challenge and document my progress through the challenge.`,
-    author: `@timnolte`,
+    title: 'Gatsby + WordPress Starter',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-wordpress',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        // The base url to your WP site.
+        baseUrl: 'wpdemo.gatsbycentral.com',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
+        protocol: 'https',
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: false,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      // Removes unused css rules
+      resolve:'gatsby-plugin-purgecss',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // Activates purging in gatsby develop
+        develop: true,
+        // Purge only the main css file
+        purgeOnly: ['/all.sass'],
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }, // must be after other CSS plugins
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
